07716082bb9941df7ce0c3887bb1b4b59683356f
Chaewon Kim <125497315+Chaewon0225@users.noreply.github.com>
2023-11-15 21:41:35 -0500

    Update UROP.py
    I set up three functions and three windows. The functions are the boxes colored in gold. I'm trying to figure out how to open and close windows.

--- UROP.py
+++ UROP.py
@@ -3,7 +3,29 @@ import pgzrun
 WIDTH = 1000
 HEIGHT = 600
 FINAL_LEVEL = 12
-START_SPEED = 10
+BACKGROUND_IMAGES = ["bedroom.png", "livingroom.png"]
+cook = Rect(500, 400, 100, 25)
+fridge = Rect(300, 200, 150, 50)
+cookbook = Rect(700, 400, 100, 25)
+functions = [cook, fridge, cookbook]
+cookw = Rect(300, 300, 300, 300)
+fridgew = Rect(300, 300, 300, 300)
+cookbookw = Rect(300, 300, 300, 300)
+windows = [cookw, fridgew, cookbookw]
+
+def draw():
+    screen.blit("bedroom", (0, 0))
+    screen.draw.filled_rect(cook, "gold")
+    screen.draw.textbox("COOK", cook, color="black"))
+    screen.draw.filled_rect(fridge, "gold")
+    screen.draw.textbox("Open the Fridge", fridge, color="black")
+    screen.draw.filled_rect(cookbook, "gold")
+    screen.draw.textbox("RECIPES", cookbook, color="black")
+    def on_mouse_down(pos):
+for cook in functions:
+            if cook.collidepoint(pos):
+                screen.draw.filled_rect(cookw, "papaya whip")
+
 
 def draw():
     screen.blit("kitchen", (0, 0))
